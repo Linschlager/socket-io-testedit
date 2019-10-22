feb4f726cf19d13f4578eea1b4ea15f473fde515
Linus Vettiger <linus4123@gmail.com>
2019-10-22 14:32:19 +0200

    Changed useMemo to useCallback

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,13 @@
-import React, { useEffect, useState, useMemo }  from 'react';
+import React, { useEffect, useState, useCallback }  from 'react';
 import io from 'socket.io-client';
 
 const App = () => {
-  const socket = useMemo(() => io('http://localhost:3000'), []);
+  const socket = useCallback(io('http://localhost:3000'), []);
   const [text, setText] = useState('');
   useEffect(() => {
-    socket.on("data", data => setText(data));
+    socket.on("data", setText);
   }, []);
+
   const onChange = (e) => {
     socket.emit('data', e.target.value);
     setText(e.target.value);
